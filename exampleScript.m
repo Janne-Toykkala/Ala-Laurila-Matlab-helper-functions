b116bba4f5c004a7d22cad3e50ac227a51db2021
Janne-Toykkala <75365986+Janne-Toykkala@users.noreply.github.com>
2021-02-18 15:44:55 +0200

    Exmple data and script initialization

---
+++ exampleScript.m
@@ -0,0 +1,15 @@
+% EXAMPLE SCRIPT TO SHOWCASE THE USE OF ALA-LAURILA LAB HELPER FUNCTIONS
+% 
+% Description - This script file will load data, plot it, and save it. The
+% used functions are getFigureWindow, getParameterValue,
+% plotSpatialStimulus, splitLogXAxis, and saveFigure.
+% 
+% Author - Janne Töykkälä, Ala-Laurila lab, Aalto University
+% 
+% Date of creation - 18th February 2021
+% _________________________________________________________________________
+
+% Clear variables and functions from memory. Close all the open figure...
+% windows. Clear command window.
+clear; close all; clc;
+
